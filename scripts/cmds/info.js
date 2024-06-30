8fd03e50af31b5a832989335294da6abf845bde1
Nonchalant557 <trixiezruc@gmail.com>
2024-06-30 21:12:17 +0800

    Update cmds

---
+++ scripts/cmds/info.js
@@ -0,0 +1,66 @@
+const moment = require('moment-timezone');
+
+module.exports = {
+  config: {
+    name: "info",
+    version: "1.0",
+    aliases: ["inf"],
+    author: "AYANFE ÑÎMÌ",
+    role: 0,
+    cooldown: 5,
+    shortDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin."
+    },
+    longDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin."
+    },
+    category: "owner",
+    guide: {
+      en: "{pn}"
+    },
+    envConfig: {}
+  },
+
+  onStart: async function ({ message, prefix }) {
+    const botPrefix = prefix; // Use the provided bot prefix
+    const authorName = "Gab Yu";
+    const authorFB = "https://www.facebook.com/100079114908948";
+
+    const now = moment();
+    const date = now.format('MMMM Do YYYY');
+    
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+    const additionalText = "";
+
+    // Combine the bot information and additional text in a single message
+message.reply(`======[𝗕𝗼𝘁 𝗶𝗻𝗳𝗼:]======
+𝗕𝗼𝘁 𝗽𝗿𝗲𝗳𝗶𝘅: ${botPrefix}
+
+𝗕𝗼𝘁 𝗻𝗮𝗺𝗲 : 𝗚𝗼𝗷𝗼 
+
+𝗢𝘄𝗻𝗲𝗿: ${authorName}
+ 
+𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: [${authorName}] ${authorFB}
+
+ 𝗗𝗮𝘁𝗲: ${date}
+
+𝗨𝗽𝘁𝗶𝗺𝗲: ${uptimeString}
+      
+      ${additionalText}
+    `);
+  },
+
+  onChat: async function ({ event, message, getLang, prefix }) {
+    if (event.body && event.body.toLowerCase() === "info") {
+      this.onStart({ message, prefix });
+    }
+  }
+};
