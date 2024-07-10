8c8fc6ba4da6dc9fdec19f9c9ef74a881737852e
GiYu4424 <trixiezruc@gmail.com>
2024-07-10 09:34:10 +0800

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,66 +1,60 @@
+const fs = require('fs');
 const moment = require('moment-timezone');
 
 module.exports = {
-  config: {
-    name: "info",
-    version: "1.0",
-    aliases: ["inf"],
-    author: "AYANFE ÑÎMÌ",
-    role: 0,
-    cooldown: 5,
-    shortDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin."
-    },
-    longDescription: {
-      vi: "",
-      en: "Sends information about the bot and admin."
-    },
-    category: "owner",
-    guide: {
-      en: "{pn}"
-    },
-    envConfig: {}
-  },
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "cliff",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const botName = "𝗚𝗶𝗬𝘂";
+		const botPrefix = "/";
+		const authorName = "𝗚𝗮𝗯𝗬𝘂";
+		const ownAge = "14";
+		const teamName = "𝗠𝗶𝘅𝗶𝗻𝖳𝖾𝖺𝗆";
+		const authorFB = "https://www.facebook.com/61562362827346";
+		const authorInsta = "Gabyu58";
+		const tikTok = "tiktok.com/@gojoyu69";
+		const urls = JSON.parse(fs.readFileSync('Gabyu.json'));
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
-  onStart: async function ({ message, prefix }) {
-    const botPrefix = prefix; // Use the provided bot prefix
-    const authorName = "Gab Yu";
-    const authorFB = "https://www.facebook.com/61562362827346";
-
-    const now = moment();
-    const date = now.format('MMMM Do YYYY');
-    
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
-
-    const additionalText = "";
-
-    // Combine the bot information and additional text in a single message
-message.reply(`======[𝗕𝗼𝘁 𝗶𝗻𝗳𝗼:]======
-𝗕𝗼𝘁 𝗽𝗿𝗲𝗳𝗶𝘅: ${botPrefix}
-
-𝗕𝗼𝘁 𝗻𝗮𝗺𝗲 : 𝗚𝗶𝘆𝘂 
-
-𝗢𝘄𝗻𝗲𝗿: ${authorName}
- 
-𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: [${authorName}] ${authorFB}
-
- 𝗗𝗮𝘁𝗲: ${date}
-
-𝗨𝗽𝘁𝗶𝗺𝗲: ${uptimeString}
-      
-      ${additionalText}
-    `);
-  },
-
-  onChat: async function ({ event, message, getLang, prefix }) {
-    if (event.body && event.body.toLowerCase() === "info") {
-      this.onStart({ message, prefix });
-    }
-  }
+		message.reply({
+			body: `《  Bot & Owner Info 》
+\Name: ${botName}
+\Bot Prefix: ${botPrefix}
+\owner: ${authorName}
+\age : ${ownAge}
+\Facebook: ${authorFB}
+\Instagram: ${authorInsta}
+\TikTok: ${tikTok}
+\Datee: ${date}
+\Time: ${time}
+\Team: ${teamName}
+\Uptime: ${uptimeString}
+\===============`,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
 };
